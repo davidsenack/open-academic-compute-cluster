868daea923680582ed322eea76fb350fb2d1aee8
David Senack <david.senack@gmail.com>
2022-04-14 16:51:39 +0000

    modify install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 #
 # Run "sudo bash ./open-academic-compute-cluster/install.sh"
 
-mv ./open-academic-compute-cluster/ /opt
+sudo cp -r ./open-academic-compute-cluster/ /opt
 cd /opt/open-academic-compute-cluster/oacc
-mv oacc.py oacc
+cp oacc.py oacc
 chmod +x oacc
 export PATH=$PATH:/opt/open-academic-compute-cluster/oacc
 cd ~ 
