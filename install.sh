1f6c00e3668e3b96908061cfdf29084f14f28c84
David Senack <david.senack@gmail.com>
2022-04-14 16:39:13 +0000

    add comments to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install the Open Academic Compute Cluster
+#
+# Run "sudo bash ./open-academic-compute-cluster/install.sh"
+
 mv ./open-academic-compute-cluster/ /opt
 cd /opt/open-academic-compute-cluster/oacc
 mv oacc.py oacc
