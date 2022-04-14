9bb7c7433cf3bcaa91c9a418c6fe245cc9414c8b
David Senack <david.senack@gmail.com>
2022-04-14 16:27:29 +0000

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mv ../open-academic-compute-cluster/ /opt
+rm -rf ../open-academic-compute-cluster
 cd /opt/open-academic-compute-cluster/oacc
 mv oacc.py oacc
 chmod +x oacc
 export PATH=$PATH:/opt/open-academic-compute-cluster/oacc
-rm -rf ../open-academic-compute-cluster 
+cd ~ 
